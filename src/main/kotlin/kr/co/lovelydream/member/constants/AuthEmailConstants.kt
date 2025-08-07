4ea5da198713454192c3ac540edbbdf2e63a7690
KoreaNirsa <islandtim@naver.com>
2025-08-07 22:29:45 +0900

    Refactor(member.email): 상수 분리

---
+++ src/main/kotlin/kr/co/lovelydream/member/constants/AuthEmailConstants.kt
@@ -0,0 +1,8 @@
+package kr.co.lovelydream.member.constants
+
+object AuthEmailConstants {
+    const val SENDER_EMAIL = "islandtim-project@naver.com"
+    const val EMAIL_SUBJECT = "[LovelyDrme] 이메일 인증 코드"
+    const val EMAIL_BODY_TEMPLATE = "요청하신 인증 코드는 [%s] 입니다.\n5분 내에 입력해주세요."
+    val CODE_RANGE = 100000..999999
+}
--- src/main/kotlin/kr/co/lovelydream/member/service/impl/AuthServiceImpl.kt
+++ src/main/kotlin/kr/co/lovelydream/member/service/impl/AuthServiceImpl.kt
@@ -2,6 +2,10 @@ package kr.co.lovelydream.member.service.impl
 
 import kr.co.lovelydream.global.enums.ResponseCode
 import kr.co.lovelydream.global.exception.AuthException
+import kr.co.lovelydream.member.constants.AuthEmailConstants.CODE_RANGE
+import kr.co.lovelydream.member.constants.AuthEmailConstants.EMAIL_BODY_TEMPLATE
+import kr.co.lovelydream.member.constants.AuthEmailConstants.EMAIL_SUBJECT
+import kr.co.lovelydream.member.constants.AuthEmailConstants.SENDER_EMAIL
 import kr.co.lovelydream.member.dto.ReqEmailDTO
 import kr.co.lovelydream.member.dto.ReqEmailVerifyDTO
 import kr.co.lovelydream.member.repository.MemberRepository
@@ -62,7 +66,7 @@ class AuthServiceImpl(
     }
 
     private fun generateCode(): String {
-        val code = (100000..999999).random().toString()
+        val code = CODE_RANGE.random().toString()
         logger.debug("랜덤 인증 코드 생성: {}", code)
         return code
     }
@@ -70,10 +74,10 @@ class AuthServiceImpl(
     private fun sendEmail(to: String, code: String) {
         logger.info("이메일 전송 시작: to={}", to)
         val message = SimpleMailMessage().apply {
-            from = "islandtim-project@naver.com"
+            from = SENDER_EMAIL
             setTo(to)
-            subject = "[LovelyDrme] 이메일 인증 코드"
-            text = "요청하신 인증 코드는 [$code] 입니다.\n5분 내에 입력해주세요."
+            subject = EMAIL_SUBJECT
+            text = EMAIL_BODY_TEMPLATE.format(code)
         }
 
         try {
